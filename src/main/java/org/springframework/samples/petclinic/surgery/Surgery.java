c0fe103fde141441795a75cb52c72b3e9353b096
Fernando Fernández <ferferga@hotmail.com>
2023-12-20 08:54:08 +0100

    test1: surgery

--- src/main/java/org/springframework/samples/petclinic/surgery/Surgery.java
+++ src/main/java/org/springframework/samples/petclinic/surgery/Surgery.java
@@ -3,17 +3,33 @@ package org.springframework.samples.petclinic.surgery;
 import java.time.LocalDate;
 import java.util.Set;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.vet.Vet;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
+@Entity
 @Setter
-public class Surgery {
+public class Surgery extends BaseEntity {
+    @NotNull
+    @NotBlank
+    @Size(min=3, max=50)
+    String name;
+    
     String description;
 
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
+    @Column(name="surgery_date")
     LocalDate date;
     
     @Transient

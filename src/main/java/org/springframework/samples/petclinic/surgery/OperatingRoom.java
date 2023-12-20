ef7cd2b30673669cb0bab08fb94b81ed188ddc88
Fernando Fernández <ferferga@hotmail.com>
2023-12-20 08:54:25 +0100

    test1: operatingroom

--- src/main/java/org/springframework/samples/petclinic/surgery/OperatingRoom.java
+++ src/main/java/org/springframework/samples/petclinic/surgery/OperatingRoom.java
@@ -2,15 +2,26 @@ package org.springframework.samples.petclinic.surgery;
 
 import java.util.Set;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-public class OperatingRoom {
+public class OperatingRoom extends BaseEntity {
+    @NotNull
+    @NotBlank
+    @Size(min=3, max=50)
+    String name;
+
     String description;
 
     @Transient
